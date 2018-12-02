56c40f4e7d9bbab8cd748d8c3865cb6698dd59a5
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-12-02 10:19:16 -0800

    Make gmp-compat-test work again on macOS.
    For whatever reason, macOS doesn't respect LD_LIBRARY_PATH, but wants to use
    DYLD_LIBRARY_PATH instead. Update the launcher script for the test to check the
    uname and set the appropriate value. I considered just setting them both, but I
    don't know if that has other implications, nor patience to find out.
    
    Fixes #26.

--- tests/gmp-compat-test/runtest.py
+++ tests/gmp-compat-test/runtest.py
@@ -90,8 +90,8 @@ def parse_args():
 
 if __name__ == "__main__":
   (options, tests) = parse_args()
-  gmp_test_so = ctypes.CDLL("gmp_test.so")
-  imath_test_so = ctypes.CDLL("imath_test.so")
+  gmp_test_so = ctypes.cdll.LoadLibrary("gmp_test.so")
+  imath_test_so = ctypes.cdll.LoadLibrary("imath_test.so")
 
   wrappers.verbose = options.verbose
   wrappers.fork    = options.fork
